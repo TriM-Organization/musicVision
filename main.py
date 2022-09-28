3eb23fa6061dbeecaafe3613e5b22374afd50f05
bgArray <474037765@qq.com>
2022-09-28 23:15:11 +0800

    补个main

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # import sheetVisionLib.use_functions
 from sheetVisionLib import use_functions
 import constants
+from bgArrayLogger import *
 
-use_functions.SheetVisionLib()
-# use_functions.pictures_path_load(constants.pictures_path)
+logger.info("program start")
+
+func_ = use_functions.SheetVisionLib()
+func_.pictures_path_load(constants.pictures_path)
+func_.pictures_reload()
+
+func_.pictures_data_load(func_.abbreviation_expansion(constants.pictures_data))
+
+func_.pictures_initialize("./sheetVisionLib/inImg/s2-1.png")
+
+func_.base_match()
+
+func_.sample_match(("升记号", "sharp"))
